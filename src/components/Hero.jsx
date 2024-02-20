e58e30ba9c51ce21ff4a5a4da4a1f3135ec9d882
cristicadis <cristi.cadis@yahoo.com>
2024-02-20 16:39:30 +0100

    Tailwind-portfolio-project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import heroImg from '../assets/hero.svg'
+import {FaGithubSquare, FaLinkedin, FaTwitterSquare} from 'react-icons/fa'
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
